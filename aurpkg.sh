91dab19ccf427f9383d76204b261be25ce3b7614
36web <36web.rocks@gmail.com>
2019-02-01 14:56:35 +0800

    make script more readable

--- aurpkg.sh
+++ aurpkg.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-sudo pacman -Sy --noconfirm
+
 USER_ID=${USER_ID:=`id -u`}
 GROUP_ID=${GROUP_ID:=`id -g`}
 PACKAGE_NAME=$1
-curl -sL https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz | tar -xz \
-&& cd $PACKAGE_NAME \
-&& makepkg -sc --noconfirm \
-&& sudo mkdir /pkg/$PACKAGE_NAME \
-&& sudo mv $1*.pkg.tar.xz PKGBUILD /pkg/$PACKAGE_NAME/ \
-&& sudo chown -R $USER_ID:$GROUP_ID /pkg/$PACKAGE_NAME
+
+sudo pacman -Sy --noconfirm
+
+curl -sL https://aur.archlinux.org/cgit/aur.git/snapshot/$PACKAGE_NAME.tar.gz | tar -xz \
+  && cd $PACKAGE_NAME \
+  && makepkg -sc --noconfirm \
+  && sudo mkdir /pkg/$PACKAGE_NAME \
+  && sudo mv $PACKAGE_NAME*.pkg.tar.xz PKGBUILD /pkg/$PACKAGE_NAME/ \
+  && sudo chown -R $USER_ID:$GROUP_ID /pkg/$PACKAGE_NAME
